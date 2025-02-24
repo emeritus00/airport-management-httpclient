edde2665d1a16e27c16fa3fa2e21fee9f898520d
Adewale Gbadamosi <adewalegbadamosi@gmail.com>
2025-02-23 22:47:03 -0330

    initial project set up and airport codes

--- src/main/java/com/keyin/http/cli/HTTPRestCLIApplication.java
+++ src/main/java/com/keyin/http/cli/HTTPRestCLIApplication.java
@@ -1,4 +1,77 @@
 package com.keyin.http.cli;
 
+import com.keyin.domain.Airport;
+import com.keyin.http.client.RESTClient;
+
+import java.util.List;
+
 public class HTTPRestCLIApplication {
+
+    private RESTClient restClient;
+
+    public String generateAirportReport() {
+        List<Airport> airports = getRestClient().getAllAirports();
+
+        StringBuffer report = new StringBuffer();
+
+        for (Airport airport : airports) {
+            report.append(airport.getName());
+            report.append(" - ");
+            report.append(airport.getCode());
+
+            if (airports.indexOf(airport) != (airports.size() - 1)) {
+                report.append(",");
+            }
+        }
+
+        System.out.println(report.toString());
+
+        return report.toString();
+    }
+
+    private void listGreetings() {
+        System.out.println(getRestClient().getResponseFromHTTPRequest());
+    }
+
+    public RESTClient getRestClient() {
+        if (restClient == null) {
+            restClient = new RESTClient();
+        }
+
+        return restClient;
+    }
+
+    public void setRestClient(RESTClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Error: No server URL provided. Please provide a server URL as a command-line argument.");
+            return; // Exit the program
+        }
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+
+        HTTPRestCLIApplication cliApp = new HTTPRestCLIApplication();
+
+        String serverURL = args[0];
+
+        if (serverURL != null && !serverURL.isEmpty()) {
+
+            RESTClient restClient = new RESTClient();
+            restClient.setServerURL(serverURL);
+
+            cliApp.setRestClient(restClient);
+
+            if (serverURL.contains("greeting")) {
+                cliApp.listGreetings();
+            } else {
+                cliApp.generateAirportReport();
+            }
+        } else {
+            System.out.println("Error: Server URL is empty. Please provide a valid server URL.");
+        }
+    }
 }
